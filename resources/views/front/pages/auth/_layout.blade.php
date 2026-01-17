15c4abda9cae376c99f72139c52a9f90a63b638e
Fajri Rinaldi Chan <fajri@gariskode.com>
2026-01-18 06:24:24 +0700

    feat: add login and authentication layout

---
+++ resources/views/front/pages/auth/_layout.blade.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <title>
+        @isset($title)
+            {{ $title }} |
+        @endisset
+        {{ $setting_web->name }}
+    </title>
+    <meta charset="utf-8" />
+    @yield('seo')
+
+    <link href="{{ asset('back/plugins/global/plugins.bundle.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('back/css/style.bundle.css') }}" rel="stylesheet" type="text/css" />
+
+</head>
+
+<body id="kt_body" class="auth-bg">
+    <script>
+        var defaultThemeMode = "light";
+        var themeMode;
+        if (document.documentElement) {
+            if (document.documentElement.hasAttribute("data-bs-theme-mode")) {
+                themeMode = document.documentElement.getAttribute("data-bs-theme-mode");
+            } else {
+                if (localStorage.getItem("data-bs-theme") !== null) {
+                    themeMode = localStorage.getItem("data-bs-theme");
+                } else {
+                    themeMode = defaultThemeMode;
+                }
+            }
+            if (themeMode === "system") {
+                themeMode = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+            }
+            document.documentElement.setAttribute("data-bs-theme", themeMode);
+        }
+    </script>
+    <div class="d-flex flex-column flex-root">
+        <div class="d-flex flex-column flex-lg-row flex-column-fluid">
+            <div class="d-flex flex-column flex-lg-row-fluid w-lg-50 p-10 order-2 order-lg-1">
+                <div class="d-flex flex-center flex-column flex-lg-row-fluid">
+                    <div class="w-lg-500px p-10">
+                        <form class="form w-100" novalidate="novalidate" id="kt_sign_in_form"
+                            data-kt-redirect-url="index.html" action="#">
+                            <div class="text-center mb-11">
+                                <h1 class="text-gray-900 fw-bolder mb-3">Sign In</h1>
+                                <div class="text-gray-500 fw-semibold fs-6">Your Social Campaigns</div>
+                            </div>
+                            <div class="row g-3 mb-9">
+                                <div class="col-md-6">
+                                    <a href="#"
+                                        class="btn btn-flex btn-outline btn-text-gray-700 btn-active-color-primary bg-state-light flex-center text-nowrap w-100">
+                                        <img alt="Logo" src="assets/media/svg/brand-logos/google-icon.svg"
+                                            class="h-15px me-3" />Sign in with Google</a>
+                                </div>
+                                <div class="col-md-6">
+                                    <a href="#"
+                                        class="btn btn-flex btn-outline btn-text-gray-700 btn-active-color-primary bg-state-light flex-center text-nowrap w-100">
+                                        <img alt="Logo" src="assets/media/svg/brand-logos/apple-black.svg"
+                                            class="theme-light-show h-15px me-3" />
+                                        <img alt="Logo" src="assets/media/svg/brand-logos/apple-black-dark.svg"
+                                            class="theme-dark-show h-15px me-3" />Sign in with Apple</a>
+                                </div>
+                            </div>
+                            <div class="separator separator-content my-14">
+                                <span class="w-125px text-gray-500 fw-semibold fs-7">Or with email</span>
+                            </div>
+                            <div class="fv-row mb-8">
+                                <input type="text" placeholder="Email" name="email" autocomplete="off"
+                                    class="form-control bg-transparent" />
+                            </div>
+                            <div class="fv-row mb-3">
+                                <input type="password" placeholder="Password" name="password" autocomplete="off"
+                                    class="form-control bg-transparent" />
+                            </div>
+                            <div class="d-flex flex-stack flex-wrap gap-3 fs-base fw-semibold mb-8">
+                                <div></div>
+                                <a href="authentication/layouts/corporate/reset-password.html"
+                                    class="link-primary">Forgot Password ?</a>
+                            </div>
+                            <div class="d-grid mb-10">
+                                <button type="submit" id="kt_sign_in_submit" class="btn btn-primary">
+                                    <span class="indicator-label">Sign In</span>
+                                    <span class="indicator-progress">Please wait...
+                                        <span class="spinner-border spinner-border-sm align-middle ms-2"></span></span>
+                                </button>
+                            </div>
+                            <div class="text-gray-500 text-center fw-semibold fs-6">Not a Member yet?
+                                <a href="authentication/layouts/corporate/sign-up.html" class="link-primary">Sign up</a>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="w-lg-500px d-flex flex-stack px-10 mx-auto">
+
+                    <div class="d-flex fw-semibold text-primary fs-base gap-5">
+                        <a href="#" target="_blank">Terms</a>
+                        <a href="#" target="_blank">Plans</a>
+                        <a href="#" target="_blank">Contact Us</a>
+                    </div>
+                </div>
+            </div>
+            <div class="d-flex flex-lg-row-fluid w-lg-50 bgi-size-cover bgi-position-center order-1 order-lg-2"
+                style="background-image: url(assets/media/misc/auth-bg.png)">
+                <div class="d-flex flex-column flex-center py-7 py-lg-15 px-5 px-md-15 w-100">
+                    <a href="index.html" class="mb-0 mb-lg-12">
+                        <img alt="Logo" src="assets/media/logos/custom-1.png" class="h-60px h-lg-75px" />
+                    </a>
+                    <img class="d-none d-lg-block mx-auto w-275px w-md-50 w-xl-500px mb-10 mb-lg-20"
+                        src="assets/media/misc/auth-screens.png" alt="" />
+                    <h1 class="d-none d-lg-block text-white fs-2qx fw-bolder text-center mb-7">Fast, Efficient and
+                        Productive</h1>
+                    <div class="d-none d-lg-block text-white fs-base text-center">In this kind of post,
+                        <a href="#" class="opacity-75-hover text-warning fw-bold me-1">the blogger</a>introduces
+                        a person they’ve interviewed
+                        <br />and provides some background information about
+                        <a href="#" class="opacity-75-hover text-warning fw-bold me-1">the interviewee</a>and
+                        their
+                        <br />work following this is a transcript of the interview.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script>
+        var hostUrl = "{{ asset('back/') }}";
+    </script>
+    <script src="{{ asset('back/plugins/global/plugins.bundle.js') }}"></script>
+    <script src="{{ asset('back/js/scripts.bundle.js') }}"></script>
+    <script src="{{ asset('back/js/custom/authentication/sign-in/general.js') }}"></script>
+    @include('sweetalert::alert')
+
+    @yield('scripts')
+</body>
+
+</html>
---
+++ resources/views/front/pages/auth/login-new.blade.php
@@ -0,0 +1,94 @@
+@extends('front.app')
+@section('seo')
+    <title>{{ $meta['description'] }}</title>
+    <meta name="description" content="{{ $meta['description'] }}">
+    <meta name="keywords" content="{{ $meta['keywords'] }}">
+    <meta name="author" content="UIN Sjech M.Djamil Djambek Bukittinggi">
+
+    <meta property="og:title" content="{{ $meta['title'] }}">
+    <meta property="og:description" content="{{ $meta['description'] }}">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="{{ route('contact.index') }}">
+    <link rel="canonical" href="{{ route('contact.index') }}">
+    <meta property="og:image" content="{{ Storage::url($meta['favicon']) }}">
+@endsection
+
+@section('content')
+    @include('front.partials.breadcrumb')
+
+    <!-- LOGIN AREA START -->
+    <div class="ltn__login-area pb-65">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="section-title-area text-center">
+                        <h1 class="section-title">{!! __('front.login_title') !!}</h1>
+                        <p>
+                            {{ $setting_web->name }}
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-lg-6">
+                    <div class="account-login-inner">
+                        <form action="{{ route('login.post') }}" class="ltn__form-box contact-form-box" method="POST" style="padding-bottom: 20px;">
+                            @csrf
+                            <div class="mb-4">
+
+                                <input class="form-control @error('login') is-invalid @enderror" type="text"
+                                    name="login" placeholder="Email/Username*" style="margin-bottom: 0px;" required>
+                                @error('login')
+                                    <small class="text-danger">{{ $message }}</small>
+                                @enderror
+                            </div>
+                            <div class="mb-4">
+
+                                <input class="form-control @error('password') is-invalid @enderror" type="password"
+                                    name="password" placeholder="Password*" style="margin-bottom: 0px;" required>
+                                @error('password')
+                                    <small class="text-danger">{{ $message }}</small>
+                                @enderror
+                            </div>
+                            <div class="btn-wrapper mt-0">
+                                <button class="theme-btn-1 btn btn-block" type="submit">{{ __('front.sign_in') }}</button>
+                            </div>
+                            <div class="go-to-btn mt-20">
+                                <a href="{{ route('password.request') }}"><small>{{ __('front.forgot_password') }}</small></a>
+                            </div>
+                        </form>
+
+                        <div style="padding: 0px 50px;">
+                            <hr style="margin: 0px 0;">
+                            <div class="btn-wrapper ">
+                                <a href="{{ route('google.redirect') }}" class="btn btn-danger btn-block"
+                                    style="background: #ea4335; color: #fff;">
+                                    <i class="fab fa-google"></i> Login with Google
+                                </a>
+                                <small class="text-muted text-center d-block mt-2">
+                                    Dengan menggunakan login Google, Anda menyetujui <a href="{{ route('privacy.policy') }}" target="_blank">Kebijakan Privasi</a> kami.
+                                </small>
+                            </div>
+                        </div>
+
+
+
+                    </div>
+                </div>
+
+                <div class="col-lg-6">
+                    <div class="account-create text-center pt-50">
+                        <h4>{{ __('auth.dont_have_account') }}</h4>
+                        <p>
+                            {{ __('auth.create_account_info') }}
+                        </p>
+                        <div class="btn-wrapper">
+                            <a href="{{ route('register') }}" class="theme-btn-1 btn black-btn">{{ __('auth.create_account') }}</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- LOGIN AREA END -->
+@endsection
